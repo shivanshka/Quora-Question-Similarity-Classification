8bd01914fddcdfc5c4846a609b74978375924249
Shivansh Kaushal <kaushal.shivansh630@gmail.com>
2023-03-10 17:35:18 +0530

    inisetup  and configs added

--- init_setup.sh
+++ init_setup.sh
@@ -4,7 +4,9 @@ conda create --prefix ./env python=3.8 -y
 echo [$(date)]: "activate environment"
 conda activate ./env
 echo [$(date)]: "create folder and file structure"
-python template.py
+#python template.py
 echo [$(date)]: "installing requirements"
 pip install -r requirements.txt -q
+echo [$(date)]: "installing spacy glove vectors"
+python -m spacy download en_core_web_lg
 echo [$(date)]: "END"
