8d3b82fdb9398bf733f482be95e8beb8e531f07d
Shivansh Kaushal <kaushal.shivansh630@gmail.com>
2023-03-10 21:10:55 +0530

    setup.py updated

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md","r",encoding = "utf-8") as f:
 
 __version__ = "0.0.1"
 
-REPO_NAME = "<Name_of_GitHub_Repo>"
+REPO_NAME = "Quora-Question-Similarity-Classification"
 AUTHOR_NAME = "Shivansh Kaushal"
 AUTHOR_USER_NAME = "shivanshka"
-SRC_REPO = "<Name_Application_in_src>"
+SRC_REPO = "Quora_App"
 AUTHOR_EMAIL = "kaushal.shivansh630@gmail.com"
 
 setuptools.setup(
@@ -16,7 +16,7 @@ setuptools.setup(
     version= __version__,
     author = AUTHOR_NAME,
     author_email= AUTHOR_EMAIL,
-    description= "",
+    description= "This application helps to detect similar questions in Quora",
     long_description=long_description,
     long_description_content = "text/markdown",
     url= f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}.git",
