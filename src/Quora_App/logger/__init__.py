1c3bad5b1fecca6e985276e59c4dd57ce02de0f8
Shivansh Kaushal <kaushal.shivansh630@gmail.com>
2023-04-12 14:48:02 +0530

    logger and main.py updated

--- src/Quora_App/logger/__init__.py
+++ src/Quora_App/logger/__init__.py
@@ -1,5 +1,6 @@
 import logging
 from datetime import datetime
+import pandas as pd
 import os
 
 LOG_DIR = "application_logs"
@@ -15,6 +16,20 @@ LOG_FILE_PATH = os.path.join(LOG_DIR,LOG_FILE_NAME)
 logging.basicConfig(filename=LOG_FILE_PATH,
 filemode="w",
 level = logging.INFO,
-format = '[%(asctime)s] [%(module)s] - %(levelname)s - %(message)s',
+format = '[%(asctime)s]|[%(module)s] | %(levelname)s | %(message)s',
 datefmt='%d-%m-%Y %H:%M:%S'
 )
+
+def get_log_dataframe(file_path):
+    data=[]
+    with open(file_path) as log_file:
+        for line in log_file.readlines():
+            data.append(line.split("|"))
+
+    log_df = pd.DataFrame(data)
+    columns=["Time stamp","Module","Level","Message"]
+    log_df.columns=columns
+    
+    log_df["log_message"] = log_df['Time stamp'].astype(str) +":$"+ log_df["Message"]
+
+    return log_df[["log_message"]]
